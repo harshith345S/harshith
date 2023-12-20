adc922eebfd44cd6fbd07a735bc0e3d88e129581
harshith345S <154422766+harshith345S@users.noreply.github.com>
2023-12-20 15:51:00 +0530

    Add files via upload

---
+++ harshith.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+		printf("enter any two number %d%d",a,b);
+		scanf("%d%d",&a,&b);
+		add=a+b;
+		printf("the addition of two numbers is:%d\n",add);
+		return 0;
+	}
+		
+		
